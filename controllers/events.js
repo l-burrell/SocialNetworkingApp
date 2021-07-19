359a69635c93bfa35f596ada6587982fce7bf327
burrell <eewinterz@gmail.com>
2021-07-19 12:27:03 -0400

    adding more routing

--- controllers/events.js
+++ controllers/events.js
@@ -2,3 +2,15 @@
 exports.locateEvent = (req, res) => {
     res.send('hi');
 }
+
+exports.addEvent = (req, res) => {
+    res.send('hia');
+}
+
+exports.updateEvent = (req, res) => {
+    res.send('hiu');
+}
+
+exports.deleteEvent = (req, res) => {
+    res.send('hid');
+}
--- controllers/users.js
+++ controllers/users.js
@@ -2,3 +2,14 @@
 exports.locateAccount = (req, res) => {
     res.send('hello')
 }
+
+exports.addAccount = (req, res) => {
+    res.send('helloa')
+}
+
+exports.updateAccount = (req, res) => {
+    res.send('hellou')
+}
+exports.deleteAccount = (req, res) => {
+    res.send('hellod')
+}
