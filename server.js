6e814c036e2a306cee8c2bd71f739cb0b3087acd
burrell <eewinterz@gmail.com>
2021-07-20 15:28:37 -0400

    trying to setup heroku

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express()
 const dot = require('dotenv')
 const handleEvents = require('./routes/events');
 const handleAccounts = require('./routes/accounts');
+const handleServices = require('./routes/client')
 
 dot.config({path: 'config.env'})
 const port = process.env.PORT || 9000;
 
+app.use('/', handleServices);
 app.use('/events', handleEvents);
 app.use('/users', handleAccounts);
 
